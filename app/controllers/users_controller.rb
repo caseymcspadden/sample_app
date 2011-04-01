245f3acd670e5fe00975d67dfae80e0b77669b2d
Casey McSpadden <casey@crossriver.com>
2011-03-31 19:18:14 -0700

    End of Chapter 10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,26 @@
 class UsersController < ApplicationController
+  before_filter :authenticate, :only => [:index, :edit, :update]
+  before_filter :correct_user, :only => [:edit, :update]  
+  before_filter :admin_user,   :only => :destroy  
+  
+  def index
+    @title = "All users"
+    @users = User.paginate(:page => params[:page])  
+  end
+  
   def show
     @user = User.find(params[:id])
     @title = @user.name
   end
   
   def new
+    redirect_to root_path if signed_in?
     @user = User.new
     @title = "Sign up"
   end
   
   def create
+    #redirect_to root_path if signed_in?  
     @user = User.new(params[:user])
     if @user.save
       sign_in @user
@@ -20,6 +31,40 @@ class UsersController < ApplicationController
       @user.password = @user.password_confirmation=""
       render 'new'
     end
+  end
+   
+  def edit
+    @title = "Edit user"
+  end
+     
+  def update
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated."
+      redirect_to @user
+    else
+      @title = "Edit user"
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_path
   end  
+  
+  private
 
+  def authenticate
+    deny_access unless signed_in?
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
+  end
+  
+  def admin_user
+    redirect_to(root_path) unless (current_user && current_user.admin?)
+  end  
 end
